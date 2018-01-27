fd098389e71b39e527a43ae520196176ad883ee9
fishies <andrewx2@uci.edu>
2018-01-27 11:35:02 -0800

    PlayerActions.cs allows controls to be easily changed in editor and in scripts

---
+++ Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerActions : MonoBehaviour {
+
+    [System.Serializable]
+    public class Action {
+        public string Player1Button; //name of button in unity input manager
+        public string Player2Button;
+        public UnityEvent Function;
+    }
+
+    public Action[] actions;
+
+    // Use this for initialization
+    void Start () {
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        foreach(Action action in actions)
+        {
+            if((action.Player1Button != "" && Input.GetButtonDown(action.Player1Button))
+            || (action.Player2Button != "" && Input.GetButtonDown(action.Player2Button)))
+            {
+                action.Function.Invoke();
+            }
+        }
+    }
+    
+    public void foo () {
+        Debug.Log("foo called");
+    }
+}
