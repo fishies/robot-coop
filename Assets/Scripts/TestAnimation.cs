743cf436f5962166a327536682988682f720d229
hlimbo <hlimbo@users.noreply.github.com>
2018-01-27 13:16:06 -0800

    import DragonBones folder
    I included an example of dragonbones animation in TestAnimation scene

---
+++ Assets/Scripts/TestAnimation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using DragonBones;
+
+public class TestAnimation : MonoBehaviour {
+    void Start() {
+        UnityFactory.factory.LoadDragonBonesData("simple_rig/SimpleDude_ske"); // DragonBones file path (without suffix) 
+        UnityFactory.factory.LoadTextureAtlasData("simple_rig/SimpleDude_tex"); //Texture atlas file path (without suffix) 
+        // Create armature. 
+        var armatureComponent = UnityFactory.factory.BuildArmatureComponent("Robo1");
+        // Input armature name 
+
+        // Play animation. 
+       armatureComponent.animation.Play("walk");
+
+
+        // Change armatureposition. 
+        armatureComponent.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+}
