364ddca1cbf6f0404fa11955cbe9968dba7f531b
Jason <dtforcep5@gmail.com>
2018-01-28 12:57:09 -0800

    Add Start screen

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,7 +11,7 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey ("escape"))
+		if (Input.GetButton("Cancel"))
 			Application.Quit ();
 	}
 }
