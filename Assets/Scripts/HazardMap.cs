21431718a50c27bccf88a3fc518b50c2dca6b73e
fishies <andrewx2@uci.edu>
2018-01-28 00:54:01 -0800

    Attach HazardMap.cs to a Tilemap of hazards. Will zap colliding objects to spawn point

---
+++ Assets/Scripts/HazardMap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardMap : MonoBehaviour {
+    public Vector2 spawnPoint;
+    Collider2D collider;
+
+    // Use this for initialization
+    void Start () {
+        collider = GetComponent<Collider2D>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    }
+
+    void OnCollisionEnter2D (Collision2D c) {
+        c.rigidbody.position = spawnPoint;
+    }
+}
