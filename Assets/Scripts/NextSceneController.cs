e8fbe299694d8c1b2684ff14064db7d7c2bf9008
Jason <dtforcep5@gmail.com>
2018-01-28 15:32:40 -0800

    Loop credits screen back to main menu

---
+++ Assets/Scripts/NextSceneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextSceneController : MonoBehaviour {
+
+	public string SceneName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Cancel"))
+			Application.Quit ();
+
+		if (Input.GetButtonDown("Start") || Input.GetButtonDown("Start2")) {
+			UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(SceneName);
+		}
+	}
+}
---
+++ Assets/Scripts/VictoryController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryController : MonoBehaviour {
+
+	public string SceneName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Start") || Input.GetButtonDown("Start2")) {
+			UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(SceneName);
+		}	
+	}
+}
