80a368a543aa13f5f616a8a740a7f540ab404ab8
fishies <andrewx2@uci.edu>
2018-01-28 02:34:38 -0800

    Calling it a night

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    public Vector2 minOffset;
+    public Vector2 maxOffset;
+
+    Collider2D collider;
+
+    // Use this for initialization
+    void Start () {
+        collider = GetComponent<Collider2D>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        transform.Translate(Vector3.up * Time.deltaTime);
+        //need to also move player with platform!
+    }
+}
