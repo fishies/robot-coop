658479902b10971eb99bcf67d67703f5c61e0cba
fishies <andrewx2@uci.edu>
2018-01-28 01:51:38 -0800

    GoalMap.cs advances to next scene when collision requirements are met

---
+++ Assets/Scripts/GoalMap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalMap : MonoBehaviour {
+
+    //every object in this array must be touching the goal tiles in order for us to advance to the next scene
+    public Collider2D [] requirements;
+    //name of next scene
+    public string sceneName;
+
+
+    Collider2D collider;
+
+    // Use this for initialization
+    void Start () {
+        collider = GetComponent<Collider2D>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        bool goalReached = true;
+        foreach(var req in requirements) {
+            goalReached &= collider.IsTouching(req);
+        }
+        if(goalReached) {
+            SceneManager.LoadSceneAsync(sceneName);
+        }
+    }
+}
